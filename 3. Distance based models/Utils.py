097b16d0cb7297f6e0ac4ffac2ff44323f05eff4
Artorias718 <lai.ndr94@gmail.com>
2023-09-20 17:38:21 +0200

    finito primo jupiter sulle distanze

--- 3. Distance based models/Utils.py
+++ 3. Distance based models/Utils.py
@@ -18,3 +18,37 @@ def find_best_k(X_train, y_train):
     best_k = grid_search.best_params_["n_neighbors"]
     best_score = grid_search.best_score_
     return best_k
+
+def tune_gamma(X_train, X_test, y_train, y_test, gamma_values):
+    best_accuracy = 0
+    best_gamma = None
+
+    for gamma in gamma_values:
+        # Create a K-NN classifier with the custom Gaussian kernel
+        knn_classifier = KNeighborsClassifier(n_neighbors=7, metric=custom_gaussian_kernel, metric_params={'gamma': gamma})
+        knn_classifier.fit(X_train, y_train)
+
+        # Make predictions on the test set
+        y_pred = knn_classifier.predict(X_test)
+
+        # Calculate the accuracy
+        accuracy = accuracy_score(y_test, y_pred)
+
+        print(f"Gamma = {gamma}, Accuracy = {accuracy:.2f}")
+
+        # Update the best gamma if the current gamma gives higher accuracy
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_gamma = gamma
+
+    print(f"Best Gamma = {best_gamma}, Best Accuracy = {best_accuracy:.2f}")
+
+    return best_gamma
+
+def custom_gaussian_kernel(x, y, gamma):
+    if x.ndim == 1:
+        x = x.reshape(1, -1)
+    if y.ndim == 1:
+        y = y.reshape(1, -1)
+    euclidean_dist = euclidean_distances(x, y)
+    return 1 - np.exp(-gamma * euclidean_dist**2)
